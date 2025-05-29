b9e7e49ccdbf845a973d7875a36f6a457f4ffb88
jrosh <jroshthen1@gmail.com>
2025-05-29 13:47:21 +0200

    fix baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-    base: String(env.VITE_BASE_URL),
+    base: '/e-inn-reader/',
     plugins: [
       vue(),
       //vueDevTools(),
